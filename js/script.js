94deb856074e476d2804c179412d506f24d69279
Bueribrian <bueribrian@hotmail.com>
2019-05-26 00:55:33 -0300

    terminado

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ window.onload = ()=>{
   loader.classList.add('loader-disabled')
  },2000)
  
+
 }
+
+let btnMenu = document.querySelector('#btn-menu')
+let exitBtn = document.querySelector('.exit-btn')
+let navMenu = document.querySelector('.nav-links')
+btnMenu.addEventListener('click',()=>{
+  navMenu.classList.toggle('flex')
+})
+exitBtn.addEventListener('click',()=>{
+ navMenu.classList.toggle('flex')
+})
